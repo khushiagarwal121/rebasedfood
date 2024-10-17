23e31fbf3bd8a7c6efa9a9e819c1e1e9020cd342
juhi <juhi.bhojani@argusoft.in>
2024-10-17 17:04:02 +0530

    Add nodemon as dev-dependency and enable zippedArchive in logs

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -9,9 +9,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const transport = new DailyRotateFile({
-  filename: "food-delivery-%DATE%.log", 
+  filename: "food-delivery-%DATE%.log",
   datePattern: "YYYY-MM-DD",
   dirname: path.join(__dirname, "../logs"), // directory where log files will be saved
+  zippedArchive: true, // old logs will be compressed to .gz format
   maxSize: "20m", // rotates log after 20mb is hit
   maxFiles: "14d", // max duration for which logs are kept
 });
